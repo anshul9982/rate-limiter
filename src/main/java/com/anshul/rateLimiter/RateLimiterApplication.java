e5e88b164c6828e7de387e9e34b927c3a2c07088
anshul <anshultyagi55562@gmail.com>
2025-11-21 15:01:03 +0530

    feat: integrate Resilience4j circuit breaker for rate limiting service with fallback handling

--- src/main/java/com/anshul/rateLimiter/RateLimiterApplication.java
+++ src/main/java/com/anshul/rateLimiter/RateLimiterApplication.java
@@ -2,8 +2,10 @@ package com.anshul.rateLimiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class RateLimiterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/anshul/rateLimiter/service/RateLimiterService.java
+++ src/main/java/com/anshul/rateLimiter/service/RateLimiterService.java
@@ -1,6 +1,9 @@
 package com.anshul.rateLimiter.service;
 
 import com.anshul.rateLimiter.strategy.RateLimiterStrategy;
+import org.slf4j.Logger;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -8,6 +11,7 @@ import reactor.core.scheduler.Schedulers;
 
 @Service
 public class RateLimiterService {
+    private static final Logger logger = LoggerFactory.getLogger(RateLimiterService.class);
     private final RateLimiterStrategy strategy;
     private final RateLimitConfigService config;
 
@@ -18,11 +22,22 @@ public class RateLimiterService {
         this.config = config;
     }
 
-    public Mono<Boolean> isAllowed(String key){
-        return Mono.fromCallable(()->config.getConfig(key)).subscribeOn(Schedulers.boundedElastic())
-                .flatMap(config->{
-                    String redisKey = "rate_limit:"+key;
+    @CircuitBreaker(name = "redisLimitBreaker", fallbackMethod = "fallbackAllowRequest")
+    public Mono<Boolean> isAllowed(String clientKey) {
+        return Mono.fromCallable(() -> config.getConfig(clientKey))
+                .subscribeOn(Schedulers.boundedElastic())
+                .flatMap(config -> {
+                    String redisKey = "rate_limit:" + clientKey;
                     return strategy.isAllowed(redisKey, config.getLimit(), config.getWindowSeconds());
                 });
+
+    }
+
+    // FALLBACK METHOD ("Fail Open")
+    // This is called when Redis is down OR the Circuit is Open
+    public Mono<Boolean> fallbackAllowRequest(String clientKey, Throwable t) {
+        logger.error("Redis unavailable/Circuit Open for client: {}. allowing request. Error: {}", clientKey, t.getMessage());
+        // Fail Open strategy: Return TRUE (Allowed)
+        return Mono.just(true);
     }
 }
